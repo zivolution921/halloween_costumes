f83132dd966f4d1da50aabb79ba239e03efda2bb
ziv <ziv921@yahoo.com>
2015-09-16 14:04:25 -0400

    first try

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(".dropdown-menu li a").click(function(){
+  var selText = $(this).text();
+  $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+});
+
